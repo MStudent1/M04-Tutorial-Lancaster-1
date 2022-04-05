d15934e8d5c9e764a3bbd03b80c463030617e638
Matthew Lancaster <98497439+MStudent1@users.noreply.github.com>
2022-04-05 01:28:13 +0000

    Added redirect for a path to prevent users from trying to enter an old path that longer exists

--- views/server.js
+++ views/server.js
@@ -10,14 +10,21 @@ const server = http.createServer((req, res) => {
 //Creat path
   let path = './views';
 switch(req.url) {
-case '/':
-	path + 'index.html';
-	break;
-case '/about':
-	path += 'about.html';
-	break;
-default:
-	path += '404.html';
+  case '/':
+	  path + 'index.html';
+    res.statusCode = 200;
+	  break;
+  case '/about':
+	  path += 'about.html';
+    res.statusCode = 200;
+	  break;
+  case '/about-me':
+    res.statusCode = 301;
+    res.setHeader('Location', '/about');
+    res.end();
+  default:
+	  path += '404.html';
+    res.statusCode = 404;
 	break;
 }
 
