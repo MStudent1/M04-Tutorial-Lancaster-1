15daf33b66e27c6efc2a99101eaf8396fe087596
Matthew Lancaster <98497439+MStudent1@users.noreply.github.com>
2022-04-05 00:29:42 +0000

    Added statements that would write html elements back to the server

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+//Set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write(<head><link rel="stylesheet" href="#"></link></head>);
+  res.write('<p>Hello, ninjas</p>');
+  res.write('<p>Hello again, ninjas</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
